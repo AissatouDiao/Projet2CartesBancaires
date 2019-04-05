b1daedc3c803a087a4b390b1345cb2948558d7c3
Satou Diao <satoudiao@gmail.com>
2019-04-05 15:54:58 +0200

    deuxieme capture du projet

--- Verificateur.js
+++ Verificateur.js
@@ -15,44 +15,54 @@ class luhn{
 		let somme2=0;
 		let somme=0;
 	
-		for(let i=2;i<T; i+=2){
+		for(let i=2;i<T+1; i+=2){
 			let A=parseInt(numero.toString().charAt(T-i));
 			let doubleN=2*A;
 			if(doubleN>9){
 				A=doubleN-9;
+				
+			}else {
+				A=doubleN;
 			}
 			 somme1=somme1+A;
       
 			}
-       //console.log("u"+somme1);
+       
 		 for (let i=1;i<T; i+=2){
 			 let B=parseInt(numero.toString().charAt(i));
+			
 			 somme2=somme2+B;
-       //i=i+2;
-		 }//console.log("j"+somme2);
+       
+		 }
 		 
 		 somme=somme1+somme2;
 		 
 		if((somme%10)==0){
-			console.log("numero de carte valide");
+			console.log("numero de carte valide...");
+			return true;
 		}else{
 			console.log("numero de carte non valide");
+			return false;
 		
 		}
 		
+	
+
+		
 	}
-} /*let A=new luhn();
-   A.Valider(5326351197617981);*/
+}  /*let A=new luhn();
+   A.Valider(48487351197617981);*/
 class Visa extends Verificateur{
     Verifiercarte(numero){
     //	super.Verifiercarte(numero);
     	numero=parseInt(numero);
     	numero=numero.toString();
+    	
     	let b=numero.substring(0,2);
     	if ((numero.length==15)&&((b==14)||(b==15))){
     		console.log("* C'est une carte Visa *");
     	}else{
-    		console.log("* Ce n'est pas une carte Visa *");
+    		console.log("* Mais ce n'est pas une carte Visa *");
     	}
         }
     }// let S=new Visa();S.Verifiercarte(177894563214587);
@@ -65,7 +75,7 @@ class MasterCard extends Verificateur{
     	if ((numero.length==16)&&((b==41)||(b==40))){
     		console.log("* C'est une carte MasterCard *");
     	}else{
-    		console.log("* Ce n'est pas une carte MasterCard *");
+    		console.log("* Mais ce n'est pas une carte MasterCard *");
     	}
         }
     }
@@ -77,7 +87,7 @@ class AmericanExpress extends Verificateur{
     	if ((numero.length==17)&&((b==39)||(b==30))){
     		console.log("* C'est une carte AmericanExpress *");
     	}else{
-    		console.log("* Ce n'est pas une carte AmericanExpress *");
+    		console.log("* Mais ce n'est pas une carte AmericanExpress *");
     	}
         }
     }
@@ -88,19 +98,30 @@ constructor(numero){
 	this.numero=numero;
 }
 getnumero(){return this.numero;}
-
-    Carte(A,B,C){ 
-    	let e=parseInt(this.getnumero());
-	A=new Visa();
-	B=new MasterCard();
-	C=new AmericanExpress();
-A.Verifiercarte(e);
-B.Verifiercarte(e);
-C.Verifiercarte(e);
-}
+valider(t){
+	t=true;
+	let A=new luhn();
+	
+	if(A.Valider(this.numero)==t){
+		//Carte(A,B,C){ 
+	    	let e=parseInt(this.getnumero());
+		let A=new Visa();
+		let B=new MasterCard();
+		let C=new AmericanExpress();
+	A.Verifiercarte(e);
+	B.Verifiercarte(e);
+	C.Verifiercarte(e);
+	//}
+	}else{
+		return false;
 	}
+}
 
-let V= new Banque(151111111111111);
-console.log("****************************************\n****************************************");
-V.Carte(new Visa,new MasterCard, new AmericanExpress );
-console.log("****************************************\n****************************************");
+    
+}
+
+	
+let V= new Banque(2255351197617981);
+console.log("************************************************");
+V.valider(new Visa,new MasterCard, new AmericanExpress );
+console.log("************************************************");
